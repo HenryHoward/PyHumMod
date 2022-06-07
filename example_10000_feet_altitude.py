b6cbe0fa14e91efb560301f19d643d3cdbe78c6f
Henry Howard <henry.a.c.howard@gmail.com>
2022-06-08 01:31:48 +1000

    added conversion script and example files

---
+++ example_10000_feet_altitude.py
@@ -0,0 +1,13 @@
+from src import hummod
+
+#set params here:
+hummod.Altitude.Feet = 10000
+
+for n in range(0, 1000):
+    hummod.step()
+    print("at time: {} seconds:\n    SV is: {}\n    heart rate is: {}\n    carotid sinus pressure is: {}\n    resp rate is {}".format(
+        hummod.System.X * 60, 
+        hummod.LeftHeartPumping_Pumping.StrokeVolume,
+        hummod.Heart_Ventricles.Rate,
+        hummod.CarotidSinus.Pressure,
+        hummod.Breathing.RespRate))
---
+++ example_defaults.py
@@ -0,0 +1,10 @@
+from src import hummod
+
+for n in range(0, 1000):
+    hummod.step()
+    print("at time: {} seconds:\n    SV is: {}\n    heart rate is: {}\n    carotid sinus pressure is: {}\n    resp rate is {}".format(
+        hummod.System.X * 60, 
+        hummod.LeftHeartPumping_Pumping.StrokeVolume,
+        hummod.Heart_Ventricles.Rate,
+        hummod.CarotidSinus.Pressure,
+        hummod.Breathing.RespRate))
